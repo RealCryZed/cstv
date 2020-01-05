d58c064cec6234913637e29a3e5ea9e06333f8c3
RealCryZed <bujhmgtnhjd@bk.ru>
2020-01-05 22:02:24 +0600

    Initial commit

---
+++ src/main/java/cstv/Controllers/MainController.java
@@ -0,0 +1,20 @@
+package cstv.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@CrossOrigin(origins = "*")
+public class MainController {
+
+    ModelAndView modelAndView = new ModelAndView();
+
+    @RequestMapping("/")
+    public ModelAndView home() {
+        modelAndView.setViewName("home");
+
+        return modelAndView;
+    }
+}
